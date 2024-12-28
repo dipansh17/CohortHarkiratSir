f224e7d83e8d475fe9201d1620b6c56234d0c78d
Dipansh <dipanshmalhotra11@gmail.com>
2024-12-28 21:46:42 +0530

    week-19 is pending will upload have some access issues

---
+++ week-20/class-1-monorepo/my-turborepo/apps/docs/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mx-auto p-4 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex flex-row justify-center" >
+      hello ji
+    </div>
+  );
+}
---
+++ week-20/class-1-monorepo/my-turborepo/apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+    <div className="flex flex-col justify-center align-middle border border-yellow-500 text-red-600 " >
+      hello ji
+      nvd
+       vd 
+    </div>
+    i want to align center above div
+    </div>
+  );
+}
